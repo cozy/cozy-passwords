20f6af79b371a0115902ba71e64a3e9a84d746ea
Cyrille Perois <cperois@outlook.com>
2019-12-06 16:32:25 +0100

    feat(PresentationPage): Add button to go to security step

--- src/components/PresentationPage.jsx
+++ src/components/PresentationPage.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import Button from 'cozy-ui/transpiled/react/Button'
+import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { Link } from 'react-router-dom'
 
-const PresentationPage = () => <div>Presentation Page</div>
+const DumbPresentationPage = props => {
+  const { t } = props
+
+  return (
+    <div>
+      <Button to="/security" label={t('PresentationPage.lets-go')} tag={Link} />
+    </div>
+  )
+}
+
+const PresentationPage = translate()(DumbPresentationPage)
 
 export default PresentationPage

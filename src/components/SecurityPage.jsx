3b39a8fcff781c4e9ab23dbfd7c504078a40fee6
Cyrille Perois <cperois@outlook.com>
2019-12-16 18:24:04 +0100

    feat(SecurityPage): Add translations

--- src/components/SecurityPage.jsx
+++ src/components/SecurityPage.jsx
@@ -3,15 +3,16 @@ import Button, { ButtonLink } from 'cozy-ui/transpiled/react/Button'
 import AppLinker, { generateWebLink } from 'cozy-ui/transpiled/react/AppLinker'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import { withClient } from 'cozy-client'
-import flowRight from 'lodash/flowRight'
+import compose from 'lodash/flowRight'
 import { Link } from 'react-router-dom'
 import Wrapper from 'components/Wrapper'
 import NarrowContent from 'cozy-ui/transpiled/react/NarrowContent'
 import strongPasswordIcon from 'assets/strong-password.svg'
 import Stack from 'cozy-ui/transpiled/react/Stack'
-import { MainTitle, Text, Bold } from 'cozy-ui/transpiled/react/Text'
+import { MainTitle, Text } from 'cozy-ui/transpiled/react/Text'
 import Card from 'cozy-ui/transpiled/react/Card'
 import { UnorderedList, ListItem } from 'cozy-ui/transpiled/react/UnorderedList'
+import snarkdown from 'snarkdown'
 
 const DumbLinkToSettings = withClient(props => {
   // eslint-disable-next-line no-unused-vars
@@ -54,7 +55,7 @@ const DumbLinkToSettings = withClient(props => {
   )
 })
 
-const LinkToSettings = flowRight(withClient, translate())(DumbLinkToSettings)
+const LinkToSettings = compose(withClient, translate())(DumbLinkToSettings)
 
 const DumbSecurityPage = props => {
   const { t } = props
@@ -64,28 +65,28 @@ const DumbSecurityPage = props => {
       <NarrowContent>
         <Stack>
           <img src={strongPasswordIcon} alt="" width="204" />
-          <MainTitle>Plus qu&apos;un seul mot de passe à retenir</MainTitle>
+          <MainTitle>{t('SecurityPage.title')}</MainTitle>
           <Stack spacing="xxl">
-            <Text>
-              Le mot de passe de votre Cozy devient l&apos;unique clé pour
-              décoder vos mots de passe sauvegardés de manières sécurisée
-            </Text>
+            <Text>{t('SecurityPage.description')}</Text>
             <Card>
-              <UnorderedList className="u-ta-left">
-                <ListItem>
-                  <Bold tag="strong">Privilégiez un mot de passe fort :</Bold>
-                  Un mot de passe long avec des chiffres et des caractères
-                  spéciaux sera impossible à casser.
-                </ListItem>
-                <ListItem>
-                  <Bold tag="strong">Mémorisez-le bien :</Bold> En cas de perte
-                  , il vous sera impossible de récupérer les mots de passe
-                  chiffrés dans votre Cozy.
-                </ListItem>
-                <ListItem>
-                  <Bold tag="strong">Notre conseil :</Bold> Trouvez une phrase
-                  courte qui vous corresponde, par exemple :
-                </ListItem>
+              <UnorderedList className="u-ta-left u-mv-0">
+                <ListItem
+                  dangerouslySetInnerHTML={{
+                    __html: snarkdown(
+                      t('SecurityPage.advices.strong_passphrase')
+                    )
+                  }}
+                />
+                <ListItem
+                  dangerouslySetInnerHTML={{
+                    __html: snarkdown(t('SecurityPage.advices.memorize'))
+                  }}
+                />
+                <ListItem
+                  dangerouslySetInnerHTML={{
+                    __html: snarkdown(t('SecurityPage.advices.our_tip'))
+                  }}
+                />
               </UnorderedList>
             </Card>
             <Stack spacing="xs">

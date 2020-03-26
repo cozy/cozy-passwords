09ad7a76f97713d99029e677830b8b1823a1cfbc
Patrick Browne <pt.browne@gmail.com>
2020-03-26 17:02:42 +0100

    feat: Add links to app stores
    Protected by a flag, so that as soon as it is available on the stores,
    we can easily switch the buttons on

--- src/components/AvailablePlatforms.jsx
+++ src/components/AvailablePlatforms.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import Card from 'cozy-ui/transpiled/react/Card'
-import Icon from 'cozy-ui/transpiled/react/Icon'
+import React, { useState, useCallback } from 'react'
 import Button, { ButtonLink } from 'cozy-ui/transpiled/react/Button'
 import supportedPlatforms from 'supportedPlatforms'
 import Stack from 'cozy-ui/transpiled/react/Stack'
-import { Text } from 'cozy-ui/transpiled/react/Text'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { isAndroid, isIOS } from 'cozy-device-helper'
+
+import Card from 'cozy-ui/transpiled/react/Card'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import { Text } from 'cozy-ui/transpiled/react/Text'
+import Modal, {
+  ModalTitle,
+  ModalFooter,
+  ModalContent,
+  ModalButtons
+} from 'cozy-ui/transpiled/react/Modal'
+
+import playBadgeEn from 'assets/google-play-badge-en.png'
+import playBadgeFr from 'assets/google-play-badge-fr.png'
+
+import appStoreBadgeEn from 'assets/app-store-badge-en.png'
+import appStoreBadgeFr from 'assets/app-store-badge-fr.png'
+import flag from 'cozy-flags'
+
+const playStoreBadges = {
+  en: playBadgeEn,
+  fr: playBadgeFr
+}
+
+const appStoreBadges = {
+  en: appStoreBadgeEn,
+  fr: appStoreBadgeFr
+}
 
 const PlatformButton = props => {
   const { icon, ...rest } = props
@@ -20,25 +44,40 @@ const PlatformButton = props => {
   )
 }
 
-const AppleStoreButton = props => {
-  return  <ButtonLink {...props}>Apple store</ButtonLink>
+const AppStoreButton = props => {
+  const { lang } = useI18n()
+  const badge = appStoreBadges[lang] || appStoreBadges.en
+  return (
+    <a {...props}>
+      <img src={badge} style={{ height: '3rem', marginRight: '0.5rem' }} />
+    </a>
+  )
 }
 
 const PlayStoreButton = props => {
-  return  <ButtonLink {...props}>Play store</ButtonLink>
+  const { lang } = useI18n()
+  const badge = playStoreBadges[lang] || playStoreBadges.en
+  return (
+    <a {...props}>
+      <img src={badge} style={{ height: '3rem' }} />
+    </a>
+  )
 }
 
 const storeLinksPerOS = {
-  ios: 'https://itunesconnect.com',
-  android: 'http://playstore.com'
+  ios: 'https://apps.apple.com/us/app/cozy-pass/id1504487449',
+  android: 'https://play.google.com/store/apps/details?id=io.cozy.pass.mobile'
 }
 
 const AvailablePlatforms = props => {
   const { t } = useI18n()
   const [isSmartphoneModalOpened, setSmartphoneModalOpened] = useState(false)
-  const handleOpenModal = useCallback(ev => {
-    setSmartphoneModalOpened(true)
-  }, [setSmartphoneModalOpened])
+  const handleOpenModal = useCallback(
+    ev => {
+      setSmartphoneModalOpened(true)
+    },
+    [setSmartphoneModalOpened]
+  )
 
   const handleDismissModal = useCallback(() => {
     setSmartphoneModalOpened(false)
@@ -57,22 +96,29 @@ const AvailablePlatforms = props => {
               label={infos.label}
             />
           ))}
-          <PlatformButton
-            icon="phone"
-            label={t('AvailablePlatforms.smartphone')}
+          {flag('passwords.app-available') ? (
+            <PlatformButton
+              icon="phone"
+              label={t('AvailablePlatforms.smartphone')}
+              href={mobileOS !== null ? storeLinksPerOS[mobileOS] : null}
+              onClick={mobileOS === null ? handleOpenModal : null}
+            />
+          ) : (
+            <PlatformButton
+              icon="phone"
+              label={t('AvailablePlatforms.smartphone-soon')}
+              disabled
+            />
+          )}
 
-            href={mobileOS !== null ? storeLinksPerOS[mobileOS] : null}
-            onClick={mobileOS === null ? handleClickSmartphone : null}
-          />
-          {isSmartphoneModalOpened ?
-            <Modal dismissAction={handleDismissModal}>
-              <ModalTitle>Please choose your plaform</ModalTitle>
-              <AppleStoreButton href={storeLinksPerOS.ios} />
-              <PlayStoreButton href={storeLinksPerOS.android} />
-              <ModalFooter>
-                <Button onClick={handleDismissModal} theme="secondary">Cancel</Button>
-              </ModalFooter>
-            </Modal> : null}
+          {isSmartphoneModalOpened ? (
+            <Modal size="small" dismissAction={handleDismissModal}>
+              <ModalContent className="u-ta-center">
+                <AppStoreButton href={storeLinksPerOS.ios} />
+                <PlayStoreButton href={storeLinksPerOS.android} />
+              </ModalContent>
+            </Modal>
+          ) : null}
         </div>
       </Stack>
     </Card>
